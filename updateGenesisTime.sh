712977c55a8e698aeaa63b56af2c327854b2ac41
Matt Marciniak <macht4tech@gmail.com>
2021-10-06 13:12:06 +0200

    l15-dev network config files (#1)
    * Prepare script for config generation
    
    * Add network name to vanguard-config
    
    * Dev config
    
    * Add config for l15-dev
    
    * Add patricia trie to pandora storage
    
    * New config
    
    * Add vanguard stats page
    
    * Changed genesis timestamp
    
    * Changed timestamp
    
    * Changed genesis time
    
    * Add genesis time update script
    
    * Provided README.md modifications;
    
    * added deposit data
    
    * Move deposit data to proper network dir
    
    * Actual configs
    
    * Change configs
    
    * Update configs
    
    * Feature: create configs
    - Added GH action config generator flow - with UI;
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Feature: create configs
    - Changed workflow file extension;
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Feature: create configs
    - Text fixes;
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Feature: create configs
    - Changed filename;
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Feature: create configs
    - Changed script path;
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Feature: create configs
    - Changed workflow filename;
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Feature: create configs
    - Debug workspace;
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Feature: create configs
    - Debug workspace;
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Feature: create configs
    - Debug workspace;
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Feature: create configs
    - Debug workspace;
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Feature: create configs
    - Debug workspace;
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Feature: create configs
    - Working implementation without GCP Storage deploy;
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Feature: create configs
    - Working implementation without GCP Storage deploy;
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Feature: create configs
    - Working implementation without GCP Storage deploy;
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Feature: create configs
    - Debug implementation without GCP Storage deploy;
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Feature: create configs
    - Debug implementation without GCP Storage deploy;
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Feature: create configs
    - Debug implementation with GCP Storage deploy;
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Feature: create configs
    - Debug implementation with GCP Storage deploy;
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Feature: create configs
    - Debug implementation with GCP Storage deploy;
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Feature: create configs
    - Debug implementation with GCP Storage deploy;
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Feature: create configs
    - Debug implementation with GCP Storage deploy;
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Feature: create configs
    - Debug implementation with GCP Storage deploy;
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Feature: create configs
    - Final implementation and test done;
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    Co-authored-by: Patryk Krakos <patryk.krakos@m8b.pl>

---
+++ updateGenesisTime.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+NETWORK=$1
+NAMESPACE=$2
+GENESIS_TIME=$3
+
+tmp=$(mktemp)
+dst="./$NETWORK/$NAMESPACE/pandora-genesis.json"
+jq '.config.pandora.genesisStartTime = '$GENESIS_TIME'' $dst >"$tmp" && mv "$tmp" $dst
+
+tmp=$(mktemp)
+dst="./$NETWORK/$NAMESPACE/vanguard-config.yaml"
+yq eval '.MIN_GENESIS_TIME = '$GENESIS_TIME $dst >"$tmp" && mv "$tmp" $dst
+
+tmp=$(mktemp)
+dst="./$NETWORK/$NAMESPACE/network-config.yaml"
+yq eval '.GENESIS_TIME = '$GENESIS_TIME $dst >"$tmp" && mv "$tmp" $dst
+
+rm -f $NETWORK/$NAMESPACE/vanguard-genesis.ssz \
+$NETWORK/$NAMESPACE/vanguard-genesis.json
+
+./genesis-state-gen-dev \
+  --deposit-json-file $NETWORK/$NAMESPACE/deposit_data.json \
+  --genesis-time $GENESIS_TIME \
+  --output-ssz $NETWORK/$NAMESPACE/vanguard-genesis.ssz \
+  --output-json $NETWORK/$NAMESPACE/vanguard-genesis.json \
+  --mainnet-config
+  echo done
+exit
