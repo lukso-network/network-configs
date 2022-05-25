df8c578f2c3ea104a06977495885c2308ba49ada
Atif Anowar <radi.cse072@gmail.com>
2022-05-25 15:30:23 +0200

    Testing docker composer for lighthouse

--- l16beta/lighthouse-docker-test/scripts/start-beacon-node.sh
+++ l16beta/lighthouse-docker-test/scripts/start-beacon-node.sh
@@ -14,6 +14,6 @@ exec lighthouse \
     --http-allow-origin="*" \
     --metrics-allow-origin="*" \
     --metrics \
-    --jwt-secrets /execution_data/geth/jwtsecret \
+    --jwt-secrets /configs/jwtsecret \
     --boot-nodes "enr:-MK4QJ170_c4shhTN7NYc0ZiumYUrME_YUcWw_d8W6KKPHtHYVTVogsTqsQZDY_R3VlWnpKW-tIMJeB2JOIvo0D3Gi6GAYDxIN1yh2F0dG5ldHOIAAAAAAAAAACEZXRoMpCBQMXLYgAAcf__________gmlkgnY0gmlwhCJaVcaJc2VjcDI1NmsxoQI6qePHCvwjkh8CMvuUaSWTNiYkFzZpZh3j-DgNjBLvo4hzeW5jbmV0cwCDdGNwgjLIg3VkcIIu4A" \
     --suggested-fee-recipient "0x7781121fd00A009670E31b76A2bf99b3A2D6878D"
