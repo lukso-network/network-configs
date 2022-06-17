8e2093dce616a413e39208c0f52b87f0ef402dc6
Atif Anowar <radi.cse072@gmail.com>
2022-06-17 10:03:43 +0200

    Configs for lh archive node

---
+++ l16/docker/lh/scripts/start-beacon-node.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+#
+# Starts a beacon node.
+
+
+exec lighthouse \
+    beacon_node \
+    --datadir /consensus_data \
+    --testnet-dir /configs \
+    --http-allow-sync-stalled \
+    --merge \
+    --execution-endpoints http://localhost:8551 \
+    --eth1-endpoints http://localhost:8545 \
+    --http \
+    --http-allow-origin="*" \
+    --http-address 0.0.0.0 \
+    --metrics-allow-origin="*" \
+    --metrics \
+    --metrics-address 0.0.0.0 \
+    --jwt-secrets /configs/jwtsecret \
+    --boot-nodes "enr:-MK4QJ170_c4shhTN7NYc0ZiumYUrME_YUcWw_d8W6KKPHtHYVTVogsTqsQZDY_R3VlWnpKW-tIMJeB2JOIvo0D3Gi6GAYDxIN1yh2F0dG5ldHOIAAAAAAAAAACEZXRoMpCBQMXLYgAAcf__________gmlkgnY0gmlwhCJaVcaJc2VjcDI1NmsxoQI6qePHCvwjkh8CMvuUaSWTNiYkFzZpZh3j-DgNjBLvo4hzeW5jbmV0cwCDdGNwgjLIg3VkcIIu4A" \
+    --suggested-fee-recipient "0x7781121fd00A009670E31b76A2bf99b3A2D6878D" \
+    --debug-level "debug"
